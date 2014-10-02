e00eef3105f9c3442df1e5d995d9d80a67a5bd56
Kris Steigerwald <kris.steigerwald@gmail.com>
2014-10-02 00:43:02 -0400

    - begin internals of calculator module - Split apart numbers vs operators

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,31 @@
 var Calculator =  (function () {
   
+  var stack = [];
+  
+  var isNumber = function (n) {
+      return !isNaN(parseFloat(n)) && isFinite(n);
+  };
+  
+  var process = function () {
+    var operators = stack.filter(isNaN),
+          numbers = stack.filter(isNumber);      
+    if(operators.length === 0) return numbers[0];
+
+  };
+  /* public */ 
+
+  var input = function (val) {
+    stack.push(val);  
+  },
+
+  equal = function () {
+    return process();
+  };
+
+  return {
+    'input' : input,
+    'equal' : equal
+  }
+
 }());
+
