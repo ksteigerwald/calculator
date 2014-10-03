1a4797b675a0778403241a3e176717846ee383e5
Kris Steigerwald <kris.steigerwald@gmail.com>
2014-10-02 21:00:05 -0400

    - Using curried functions to build input calculations. This is a stash commit   more or less, cleanup/refactor commits to follow

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,25 @@
 var Calculator =  (function () {
   
-  var stack = [];
+  var stack = [], memory = 0, tail = 0;
 
   var operations = {
-    add: function(a, b) { return a + b; },
-    subtract: function (a, b) { return a - b; } 
+    value: function (val) {
+      return function () {
+        return val; 
+      };  
+    },
+
+    add: function(a) { 
+      return function(b) { 
+        return a + b; 
+      };
+    },
+
+    subtract: function (a) { 
+      return function (b) {
+        return a - b; 
+      }; 
+    } 
   };
   
   var isNumber = function (n) {
@@ -20,27 +35,42 @@ var Calculator =  (function () {
     var operators = stack.filter(isNaN),
           numbers = stack.filter(isNumber);      
     if(operators.length === 0) return last();
+    //console.log(numbers,'::', stack,stack.indexOf('add'));
     //return operations.add.apply(this,numbers);
-    //return numbers.reduce(operations.add);
-    return numbers.reduce(operations.subtract);
+    return curry(operations.add,'add')(numbers);
+    //return numbers.reduce(operations.subtract);
   };
+
+  var commulate = function (val) {
+    return (memory !== 0 && stack.lenght === 0) ? val + memory : val;  
+  };
+
 
   /* public */ 
 
   var input = function (val) {
-    stack.push(val);  
+    if(isNumber(val)){
+      if(stack.length > 0) {
+        memory = stack[0](val);
+        stack = [];
+      }
+      else{
+        memory = val;
+      }
+    }
+    else {
+      stack.push(operations[val](memory));  
+    }
+    console.log("input:".concat(val, " memory:", memory, " stack:", stack));
   },
 
   equal = function () {
-
-    var total = compute();
-        stack = [total];
-
-    return total;
+    return memory;
   },
 
   clear = function () {
     stack = []; 
+    memory = 0;
     return 0;
   };
   
--- test/calculator_test.js
+++ test/calculator_test.js
@@ -10,33 +10,34 @@ test("Clear", function() {
 });
 
 test('Recives Input, returns output', function() { 
-  Calculator.input(12);
-  strictEqual(Calculator.equal(), 12);
-  Calculator.input(15);
-  strictEqual(Calculator.equal(), 15);
+ // Calculator.input(12);
+ // strictEqual(Calculator.equal(), 12);
+ // Calculator.input(15);
+ // strictEqual(Calculator.equal(), 15);
 });
 
 test("Add", function() {
   Calculator.input(1); 
   Calculator.input('add'); 
   Calculator.input(1); 
-  strictEqual(Calculator.equal(), 2);
-  
+  strictEqual(Calculator.equal(), 2, '1 + 1 = 2');
+  Calculator.input('add'); 
   Calculator.input(51); 
   Calculator.input('add'); 
   Calculator.input(11); 
-  strictEqual(Calculator.equal(), 64);
+  strictEqual(Calculator.equal(), 64, '2 + 51 + 11 = 64');
   Calculator.input('add'); 
   Calculator.input(10); 
-  strictEqual(Calculator.equal(), 74);
+  strictEqual(Calculator.equal(), 74, '64 + 10 = 74');
 });
 
 test("Subtract", function() {
   Calculator.input(10); 
   Calculator.input('subtract'); 
   Calculator.input(5);
-  strictEqual(Calculator.equal(), 5);
+  strictEqual(Calculator.equal(), 5, '10 - 5 = 5');
+  Calculator.input('subtract'); 
   Calculator.input(2);
   Calculator.input('subtract'); 
-  strictEqual(Calculator.equal(), 3);
+  strictEqual(Calculator.equal(), 3, '2 - 3');
 });
